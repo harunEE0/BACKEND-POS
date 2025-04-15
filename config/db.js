6d88ed2a017256c7991f922b7064cae9bca3a428
harunEE0 <harun0936723724@hotmail.com>
2025-04-15 12:12:41 +0700

    update db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,39 @@
 /**JWT/config/db.js */
 const mongoose = require('mongoose');
+ const logger = require('../utills/logger');
 
 const connectDB = async () => {
-    try{
-        await mongoose.connect(process.env.MONGO_URI||'mongodb://localhost:27017/BackendPoS');
-        console.log("MongoDB connected");
+    try{const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pos', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 5000, // 5 วินาที
+        socketTimeoutMS: 45000, // 45 วินาที
+        maxPoolSize: 10, // Connection Pool
+        retryWrites: true,
+        w: 'majority'
+      });
+  
+      logger.info(`MongoDB Connected: ${conn.connection.host}`);
+  
+      // Event Listeners
+      mongoose.connection.on('connected', () => {
+        logger.info('MongoDB connection established');
+      });
+  
+      mongoose.connection.on('error', (err) => {
+        logger.error(`MongoDB connection error: ${err.message}`);
+      });
+  
+      mongoose.connection.on('disconnected', () => {
+        logger.warn('MongoDB connection disconnected');
+      });
+  
+      process.on('SIGINT', async () => {
+        await mongoose.connection.close();
+        logger.info('MongoDB connection closed due to app termination');
+        process.exit(0);
+      });
+  
     } catch(err){
         console.log('MONGO error',err);
         process.exit(1);
