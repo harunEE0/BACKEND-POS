ac6795901f26858354609b5b3bdafe477084a264
harunEE0 <harun0936723724@hotmail.com>
2025-05-03 02:31:47 +0700

    *-*

--- services/authService.js
+++ services/authService.js
@@ -1,67 +1,58 @@
 /**E:\learn-code\backend-pos\services\authService.js */
 
-
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-exports.registerUser = async ({ username, password, role }) => {
-  if (!username || !password || !role) {
-    const error = new Error('Please provide username, password and role');
-    error.statusCode = 400;
-    throw error;
+class AuthService {
+  constructor() {
+    this.userModel = User;
   }
 
-  try {
-    return await User.create({ username, password, role });
-  } catch (err) {
-    if (err.code === 11000) {
-      const error = new Error('Username already exists');
-      error.statusCode = 400;
-      throw error;
+  async register({ username, password, role }) {
+    if (!username || !password || !role) {
+      throw new Error('Please provide username, password and role');
     }
-    throw err;
-  }
-};
-exports.generateRefreshToken = async (userId) => {
-  const refreshToken = jwt.sign(
-    { id: userId },
-    process.env.REFRESH_TOKEN_SECRET,
-    { expiresIn: '7d' }
-  );
-  
-  await User.findByIdAndUpdate(userId, { refreshToken });
-  return refreshToken;
-};
 
-exports.validateUserLogin = async ({ username, password }) => {
-  if (!username || !password) {
-    const error = new Error('Please provide username and password');
-    error.statusCode = 400;
-    throw error;
+    try {
+      return await this.userModel.create({ username, password, role });
+    } catch (err) {
+      if (err.code === 11000) {
+        const error = new Error('Username already exists');
+        error.statusCode = 400;
+        throw error;
+      }
+      throw err;
+    }
   }
 
-  const user = await User.findOne({ username }).select('+password');
-  if (!user) {
-    const error = new Error('Invalid credentials');
-    error.statusCode = 401;
-    throw error;
+  async login({ username, password }) {
+    if (!username || !password) {
+      throw new Error('Please provide username and password');
+    }
+
+    const user = await this.userModel.findOne({ username }).select('+password');
+    if (!user) throw new Error('Invalid credentials');
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) throw new Error('Invalid credentials');
+
+    return user;
   }
 
-  const isMatch = await user.comparePassword(password, user.password);
-  if (!isMatch) {
-    const error = new Error('Invalid credentials');
-    error.statusCode = 401;
-    throw error;
+  async generateToken(userId) {
+    return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
   }
 
-  return user;
-};
-
-exports.getUserById = async (id) => {
-  const user = await User.findById(id);
-  if (!user) {
-    const error = new Error('User not found');
-    error.statusCode = 404;
-    throw error;
+  async generateRefreshToken(userId) {
+    const refreshToken = jwt.sign(
+      { id: userId },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: '7d' }
+    );
+    await this.userModel.findByIdAndUpdate(userId, { refreshToken });
+    return refreshToken;
   }
-  return user;
-};
+}
+
+module.exports = new AuthService();
+
